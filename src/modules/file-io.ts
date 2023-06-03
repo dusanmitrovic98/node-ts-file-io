19e32151951e5074593ac1ee0e2b22a9c68d6ae3
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-03 17:47:56 +0200

    Update file-io.ts

--- src/modules/file-io.ts
+++ src/modules/file-io.ts
@@ -4,4 +4,6 @@ export function readFile(filePath: string): string {
   return fs.readFileSync(filePath, "utf8");
 }
 
-export function writeFile(filePath: string, content: string): void {}
+export function writeFile(filePath: string, content: string): void {
+  fs.writeFileSync(filePath, content);
+}
