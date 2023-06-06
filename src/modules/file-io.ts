f1edbc80c3efc6c66a16839868e968a78d61396a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-06 18:31:19 +0200

    Update file-io.ts

--- src/modules/file-io.ts
+++ src/modules/file-io.ts
@@ -1,1 +1,1 @@
-import fs from "fs";export function readFile(filePath: string): string {  return fs.readFileSync(filePath, "utf8");}export function readFileLines(  filePath: string,
+import fs from "fs";export function readFile(filePath: string): string {  return fs.readFileSync(filePath, "utf8");}export function readFileLines(  filePath: string,  start?: number,
